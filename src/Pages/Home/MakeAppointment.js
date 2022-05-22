e256d0617881f9e6ad8de6544d2daa004531aa4d
Bin-Amin <hmsolaiman6@gmail.com>
2022-05-22 21:27:40 +0600

    doctor image heade overlap done

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import doctor from '../../assets/images/doctor.png'
+import appointment from '../../assets/images/appointment.png'
+import PrimaryButton from '../Shared/PrimaryButton'
+
+const MakeAppointment = () => {
+    return (
+        <section style={{
+            background: `url(${appointment})`
+        }}
+            className='flex justify-center items-center'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-130px]' src={doctor} alt="" />
+            </div>
+            <div className='flex-1'>
+                <h2 className='text-xl text-primary font-bold '>Appointment</h2>
+                <h2 className='text-3xl text-white py-6'>Make an Appointment Today</h2>
+                <p className='text-white py-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos at repellat vitae iure a veritatis, totam reprehenderit impedit ex, praesentium sit ad nulla ab nam in, et natus incidunt?
+                    Et inventore iusto voluptates repellendus ipsam nam magnam optio atque perferendis ad! Minus ipsa voluptate rerum quos officia architecto doloribus tempora doloremque laboriosam veritatis nemo ratione impedit error, aperiam rem?
+                    Nam optio porro voluptatum, iusto, unde neque illo itaque magnam earum deleniti nemo facilis. Laudantium accusamus perferendis ea ut omnis adipisci, est expedita rem ducimus necessitatibus autem libero eligendi labore!</p>
+                <PrimaryButton>Get Started</PrimaryButton>
+            </div>
+        </section>
+    )
+}
+
+export default MakeAppointment
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import treatment from '../../assets/images/treatment.png';
+import Service from './Service';
+import PrimaryButton from '../Shared/PrimaryButton';
+
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: 'Fluoride Treatmant',
+            description: '',
+            img: fluoride
+        },
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            description: '',
+            img: cavity
+        },
+        {
+            _id: 3,
+            name: 'Teeth Whitening',
+            description: '',
+            img: whitening
+        },
+    ];
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h2 className='text-primary text-xl font-bold uppercase'>Our Services</h2>
+                <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service key={service._id} service={service}></Service>)
+                }
+            </div>
+            <div>
+                <div class="hero min-h-screen">
+                    <div class="hero-content flex-col lg:flex-row">
+                        <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                        <div className='px-16'>
+                            <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms!</h1>
+                            <p class="py-8">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                            <PrimaryButton>Get Started</PrimaryButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services
