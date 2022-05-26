fa59dd5bbebd05ec2957725b0b15a79045e62b46
Bin-Amin <hmsolaiman6@gmail.com>
2022-05-26 16:19:44 +0600

    login form font en compelite

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,8 +1,11 @@
 import { format } from 'date-fns'
 import React, { useEffect, useState } from 'react'
+import BookingModal from './BookingModal';
+import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
         fetch('services.json')
@@ -13,6 +16,18 @@ const AvailableAppointments = ({ date }) => {
     return (
         <div>
             <h3 className='text-xl text-secondary text-center'> Available Appointments on {format(date, 'PP')}</h3>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service key={service._id} service={service}
+                        setTreatment={setTreatment}
+                    />)
+                }
+            </div>
+            {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}
+            />}
         </div>
     )
 }
